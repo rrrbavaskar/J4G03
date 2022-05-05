f35576a6fd30db2450012a66aaee852dda7c43d3
Rutuja Ravindra Bavaskar <102590124+rrrbavaskar@users.noreply.github.com>
2022-05-05 21:46:51 +0530

    Add files via upload

--- Rutuja Ravindra Bavasakar/Day-27/app.component.spec.ts
+++ Rutuja Ravindra Bavasakar/Day-27/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tdf-form'`, () => {
+  it(`should have as title 'job-form'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tdf-form');
+    expect(app.title).toEqual('job-form');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('tdf-form app is running!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('job-form app is running!');
   });
 });
